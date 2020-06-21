d6f6754e71f27822649dbeb666947ff612126f2b
Rachmat Wachid <rwachid17@gmail.com>
2020-06-21 16:57:56 +0700

    Replace adminpro to adminlte in project

--- travel_project/routes/web.php
+++ travel_project/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('travel_package','TravelController');
+Route::resource('gallery','GalleryController');
+Route::resource('transaction','TransactionController');
