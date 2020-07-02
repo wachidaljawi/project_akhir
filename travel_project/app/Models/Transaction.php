664965055d306e63f7f4b5f3effaae72f291604d
Rachmat Wachid <rwachid17@gmail.com>
2020-07-02 10:50:40 +0700

    create view pages admin transaction

--- travel_project/app/Models/Transaction.php
+++ travel_project/app/Models/Transaction.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function detail(){
+        return $this->hasMany( Transaction_details::class, 'transactions_id', 'id' );
+    }
+
+    public function travel_package(){
+        return $this->belongsTo( Travel_package::class, 'travel_packages_id', 'id' );
+    }
+
+    public function user(){
+        return $this->belongsTo( User::class, 'users_id', 'id' );
+    }
 }
